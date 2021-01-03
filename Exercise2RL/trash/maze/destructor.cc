96038e852bd49661ad06f2b0639847c0a26d7b21
TSwagerman <thomasswagerman@hotmail.com>
2021-01-03 23:28:05 +0100

    smth

--- Exercise2RL/trash/maze/destructor.cc
+++
@@ -1,8 +0,0 @@
-//#define XERR
-#include "maze.ih"
-
-maze::~Maze()
-{
-    cout << "Destroyed!" << endl;
-    delete[] d_mazeGrid;
-}
--- Exercise2RL/trash/maze/maze1.cc
+++
@@ -1,7 +0,0 @@
-//#define XERR
-#include "maze.ih"
-
-maze::Maze()
-//:
-{
-}
